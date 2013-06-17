399fa4d6a9e67c6feb9f39663730363f256aa1a7
Elijah Boston <elijah@fuzzydynamics.com>
2013-06-17 00:53:31 -0400

    Let's get the party started.

---
+++ js/main.js
@@ -0,0 +1,20 @@
+// main.js
+$('h1.open-content').click(function(event) {
+	var parent = $(this).parent().parent('article'),
+		child = parent.children('.reveal');
+
+	if (parent.hasClass('active')) {
+		// close
+		parent.removeClass('active');
+		child.css('height', '0');
+	} else {
+		// open
+
+		// close all other tabs
+		$('article').removeClass().children('.reveal').css('height','0');
+
+		// open this tab
+		parent.addClass('active');
+		child.css('height', child.children('.content').innerHeight() + 40);
+	}
+});
